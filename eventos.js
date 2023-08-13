146b85adc37174c26ea219f61168422e30c16dd0
Gaston Britos <gaston.b17@gmail.com>
2023-08-13 18:54:19 -0300

    solicion click

--- eventos.js
+++ eventos.js
@@ -5,3 +5,18 @@ miBloque.addEventListener("click", saludar);
 function saludar(){
     alert("Hola! Soy el div");
 }
+// al hacer clik en mi boton actua la función que me elimina el evento del div por lo que no me salta su alert.
+let noDiv = document.getElementById("btn");
+
+function cancelDiv(){
+    miBloque.removeEventListener("click", saludar);
+}
+
+noDiv.addEventListener("click", cancelDiv);
+
+// creo un evento en el div que me restaura el alert anteriormente eliminado.
+function returnDiv(){
+    miBloque.addEventListener("click", saludar); 
+}
+
+miBloque.addEventListener("click", returnDiv);
